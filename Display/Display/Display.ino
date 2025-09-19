0197443877c2c8a61bc17bdd4d52b33c05669bd1
JUAN ROMAN OTERO OLLER <48417781@ort>
2025-09-19 10:11:58 -0300

    Correccion

--- Display/Display/Display.ino
+++ Display/Display/Display.ino
@@ -1,8 +1,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// Dirección I2C de la pantalla LCD (puede variar: 0x27 o 0x3F)
-// Si no estás seguro, usá el código scanner que ya tenés
+
 #define LCD_ADDR 0x27  
 
 // Creamos objeto LCD: (dirección, columnas, filas)
@@ -13,17 +12,13 @@ LiquidCrystal_I2C lcd(LCD_ADDR, 16, 2);
 #define SCL_PIN 22
 
 void setup() {
-  // Iniciar comunicación serie
   Serial.begin(115200);
-
-  // Inicializamos I2C1 en los pines definidos
   Wire.begin(SDA_PIN, SCL_PIN);
 
-  // Inicializamos la pantalla LCD
-  lcd.init();
+  
+  lcd.init();         // Inicializamos la pantalla LCD
   lcd.backlight();  // Encender luz de fondo
 
-  // Mensaje inicial
   lcd.setCursor(0, 0);
   lcd.print("   Hola, Roman!");
 
@@ -32,7 +27,6 @@ void setup() {
 }
 
 void loop() {
-  // Animación simple: contador en la segunda línea
   static int contador = 0;
 
   lcd.setCursor(0, 1);
